9ed2221a08423388bfc14315d5f62809f579f4fd
IanFanYT <andre.purnama2283@gmail.com>
2023-01-12 02:25:21 +0000

    add and update

--- lib/admin/login_admin.dart
+++ lib/admin/login_admin.dart
@@ -1,10 +1,47 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class LoginAdmin extends StatelessWidget {
+class LoginAdmin extends StatefulWidget {
   const LoginAdmin({Key? key}) : super(key: key);
 
   @override
+  State<LoginAdmin> createState() => _LoginAdminState();
+}
+
+class _LoginAdminState extends State<LoginAdmin> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Container(
+        child: Column(children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 50),
+            child: Image.asset(
+              'name',
+              height: null,
+              width: null,
+            ),
+          ),
+          Expanded(
+              child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 32),
+            constraints: BoxConstraints.expand(),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(35),
+                    topRight: Radius.circular(35))),
+            child: Column(children: [
+              SizedBox(
+                height: 50,
+              ),
+              Text('Sign Up',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold))
+            ]),
+          )),
+        ]),
+      ),
+    );
   }
 }
--- lib/login_menu.dart
+++ lib/login_menu.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pencariandataalumni/admin/login_admin.dart';
+import 'package:pencariandataalumni/guru/login_guru.dart';
+import 'package:pencariandataalumni/user/login_user.dart';
 
 class loginmenu extends StatelessWidget {
   const loginmenu({Key? key}) : super(key: key);
@@ -8,13 +11,13 @@ class loginmenu extends StatelessWidget {
     final MediaQueryWidth = MediaQuery.of(context).size.width;
     final bodyheight = MediaQueryHeight - MediaQuery.of(context).padding.top;
     return Scaffold(
+      backgroundColor: Colors.blue.shade100,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Center(
             child: Container(
-              color: Colors.white,
               height: bodyheight * 0.3,
               child: Image(
                 image: AssetImage("assets/loginmenu.png"),
@@ -24,15 +27,45 @@ class loginmenu extends StatelessWidget {
           SizedBox(
             height: 10,
           ),
-          ElevatedButton(
-            child: const Text('Enabled Button'),
-            onPressed: () {},
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('data'),
-          ),
-          ElevatedButton(onPressed: () {}, child: const Text('data')),
+          Container(
+              height: bodyheight * 0.1,
+              width: MediaQueryWidth,
+              padding: const EdgeInsets.all(5),
+              child: ElevatedButton(
+                child: const Text('ALUMNI'),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (contex) => const LoginUser()));
+                },
+              )),
+          Container(
+              height: bodyheight * 0.1,
+              width: MediaQueryWidth,
+              padding: const EdgeInsets.all(5),
+              child: ElevatedButton(
+                child: const Text('GURU'),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (contex) => const LoginGuru()));
+                },
+              )),
+          Container(
+              height: bodyheight * 0.1,
+              width: MediaQueryWidth,
+              padding: const EdgeInsets.all(5),
+              child: ElevatedButton(
+                child: const Text('ADMIN'),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (contex) => const LoginAdmin()));
+                },
+              )),
         ],
       ),
     );
